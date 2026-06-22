94639d570b063d5e257d3f6557907058a204fc9d
Mark-Justine-Go <118537589+Mark-Justine-Go@users.noreply.github.com>
2026-06-22 17:10:24 +0800

    feat: add the knightmoves function

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# knight-travails
+# Knight Travails
+
+This project consists of 1 function that shows the shortest possible way to get from one square to another in a chessboard using a knight chess piece.
+
+The data structure used in this project is a graph to represent the possible moves in a chessboard that a knight can take. The algorithm used is BFS (Breadth-First-Search). BFS explores all the nodes(possible moves) at move 1 before moving on to the next move and so on. The next time it reaches the right node (target square), its guaranteed to be the shortest path due to the nature of the algorithm.
+
+Usage example: 
+knightmove([3,3],[0,0]) - [3,3] being the first position where the knight is and [0,0] being the position the knight should find the shortest path to.
--- script.js
+++ script.js
@@ -13,3 +13,35 @@ function getPossibleMoves(x,y,boardSize = 7){
 
     return validMoves;
 }
+
+function knightMoves(start,end){    
+    let queue = [start];
+    let visited = new Set();
+    visited.add(JSON.stringify(start));
+    const parent = {};
+
+    while(queue.length > 0){
+        const currOffset = queue.shift();
+
+        if(JSON.stringify(currOffset) === JSON.stringify(end)) break;
+        
+        for(let possibleMove of getPossibleMoves(currOffset[0],currOffset[1])){
+            if(!visited.has(JSON.stringify(possibleMove))){
+                visited.add(JSON.stringify(possibleMove));
+                parent[possibleMove] = currOffset;
+                queue.push(possibleMove);
+            }
+        }
+    }
+
+    const path = [];
+    let current = end;
+
+    while(current !== undefined){
+        path.push(current);
+        current = parent[current];
+    }
+
+    console.log(`You made it in ${path.length} moves! Heres your path: `);
+    path.reverse().forEach(pos => console.log(pos));
+}
